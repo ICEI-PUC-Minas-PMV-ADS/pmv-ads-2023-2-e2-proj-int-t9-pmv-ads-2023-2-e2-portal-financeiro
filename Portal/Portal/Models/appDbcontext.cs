244b87a56669c51ac6090bd33020512a20dd60bb
Mario Filho <119667986+mariojcfilho@users.noreply.github.com>
2023-10-24 20:05:59 -0300

    Add files via upload

---
+++ Portal/Portal/Models/appDbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PORTAL.Models
+{
+    public class AppDbcontext : DbContext
+
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options) { }
+
+        public DbSet<Despesa> Despesas { get; set; }
+
+    }
+}
