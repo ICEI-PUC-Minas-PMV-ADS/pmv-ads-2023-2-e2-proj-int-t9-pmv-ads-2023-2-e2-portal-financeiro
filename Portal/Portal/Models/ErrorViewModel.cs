e1d2fa5a97825c25278391a28aa2b10d2538b0b4
Lorena <128756484+LorenaPoli@users.noreply.github.com>
2023-10-31 22:42:27 -0300

    Delete Portal directory

--- Portal/Portal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PORTAL.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
