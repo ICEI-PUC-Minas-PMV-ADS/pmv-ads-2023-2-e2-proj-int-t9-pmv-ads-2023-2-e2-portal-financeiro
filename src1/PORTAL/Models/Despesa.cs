076861c3d70bdf5ed453d066a37e4b83875702e9
Mario Filho <119667986+mariojcfilho@users.noreply.github.com>
2023-11-05 16:16:27 -0300

    arquivos teste novo src

---
+++ src1/PORTAL/Models/Despesa.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PORTAL.Models
+{
+    [Table("Despesas")]
+
+    public class Despesa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a conta")]
+
+        public string Conta { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o mês")]
+      
+        public string Mês { get; set; }
+
+
+
+
+
+    }
+}    
