0e36a220e630ac497b401ce0c11e99b6bfa43e5e
Mario Filho <119667986+mariojcfilho@users.noreply.github.com>
2023-12-05 19:03:04 -0300

    Delete src2 directory
    removendo diretorio invalido

--- src2/Portal-financeiro2023/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Portal_financeiro2023.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
